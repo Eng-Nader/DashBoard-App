edf5cd273a5d23f8763627fe3cf7567110130e90
NaderSameer <nadersameer@std.mans.edu.eg>
2025-08-28 08:36:41 +0300

    create a AppImage assets and finish it

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,31 @@
+
+class AppImage {
+  AppImage._();
+  
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  static const String imagesCard = "assets/images/card.svg";
+
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  static const String imagesExpencess = "assets/images/expencess.svg";
+
+  static const String imagesIncome = "assets/images/income.svg";
+
+  static const String imagesInvesment = "assets/images/invesment.svg";
+
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  static const String imagesMoondark = "assets/images/moondark.svg";
+
+  static const String imagesMoonlight = "assets/images/moonlight.svg";
+
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  static const String imagesStatics = "assets/images/statics.svg";
+
+  static const String imagesTransaction = "assets/images/transaction.svg";
+
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
+
