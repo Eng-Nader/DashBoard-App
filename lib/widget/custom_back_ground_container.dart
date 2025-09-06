ad06f30a01180b15bd8e0dc0015946b0549baccb
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-06 04:41:22 +0300

    add padding value for a customBackGround Conatiner

--- lib/widget/custom_back_ground_container.dart
+++ lib/widget/custom_back_ground_container.dart
@@ -4,13 +4,15 @@ class CustomBackGroundContainer extends StatelessWidget {
   const CustomBackGroundContainer({
     super.key,
     required this.child,
+    required this.paddingValue,
   });
   final Widget child;
+  final double paddingValue;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(20),
+      padding: EdgeInsets.all(paddingValue),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(16),
--- lib/widget/custom_expnses.dart
+++ lib/widget/custom_expnses.dart
@@ -1,6 +1,3 @@
-import 'package:dashboarda_app/models/expenses_model.dart';
-import 'package:dashboarda_app/utils/app_images.dart';
-import 'package:dashboarda_app/widget/all_expenses_item.dart';
 import 'package:dashboarda_app/widget/all_expenses_item_list_view.dart';
 import 'package:dashboarda_app/widget/custom_back_ground_container.dart';
 import 'package:dashboarda_app/widget/expnses_header.dart';
@@ -12,6 +9,7 @@ class CustomExpnses extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const CustomBackGroundContainer(
+      paddingValue: 20,
       child: Column(
         children: [
           ExpensesHeader(),
@@ -24,4 +22,3 @@ class CustomExpnses extends StatelessWidget {
     );
   }
 }
-
