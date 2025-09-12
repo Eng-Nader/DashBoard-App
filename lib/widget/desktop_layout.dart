369f4909217e77759f9cad3d846647db8dbf6ca4
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-12 06:20:45 +0300

    create a Quick invoke header

--- lib/widget/desktop_layout.dart
+++ lib/widget/desktop_layout.dart
@@ -1,5 +1,8 @@
+import 'package:dashboarda_app/widget/all_expenses_item_list_view.dart';
+import 'package:dashboarda_app/widget/custom_back_ground_container.dart';
 import 'package:dashboarda_app/widget/custom_drawar.dart';
 import 'package:dashboarda_app/widget/custom_expnses.dart';
+import 'package:dashboarda_app/widget/quick_invoice.dart';
 import 'package:flutter/material.dart';
 
 class DesptopLayout extends StatelessWidget {
@@ -18,14 +21,14 @@ class DesptopLayout extends StatelessWidget {
             width: 30,
           ),
           Expanded(
-              flex: 2,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: CustomExpnses(),
-                  )
-                ],
-              )),
+            flex: 2,
+            child: Column(
+              children: [
+                CustomExpnses(),
+                QuickInvoice(),
+              ],
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/widget/quick_invoice.dart
@@ -0,0 +1,45 @@
+import 'package:dashboarda_app/utils/app_style.dart';
+import 'package:dashboarda_app/widget/custom_back_ground_container.dart';
+import 'package:flutter/material.dart';
+
+class QuickInvoice extends StatelessWidget {
+  const QuickInvoice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomBackGroundContainer(
+        paddingValue: 20,
+        child: Column(
+          children: [
+            QuickInvoiceHeader(),
+          ],
+        ));
+  }
+}
+
+class QuickInvoiceHeader extends StatelessWidget {
+  const QuickInvoiceHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Text(
+          'Quick Invoice',
+          style: AppStyle.styleSemibold20,
+        ),
+        Expanded(
+          child: SizedBox(),
+        ),
+        CircleAvatar(
+          radius: 30,
+          backgroundColor: Color(0xffFAFAFA),
+          child: Icon(
+            Icons.add,
+            color: Color(0xff4EB7F2),
+          ),
+        )
+      ],
+    );
+  }
+}
