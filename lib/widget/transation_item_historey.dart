76e234a51661f21de2fdbd8f1a70fb14b0bcab81
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-13 07:38:11 +0300

    create a transaion item history

--- lib/widget/transation_item_historey.dart
+++ lib/widget/transation_item_historey.dart
@@ -1,10 +1,29 @@
+import 'package:dashboarda_app/core/utils/app_style.dart';
+import 'package:dashboarda_app/models/tansation_history_model.dart';
 import 'package:flutter/material.dart';
 
 class TransationItem extends StatelessWidget {
-  const TransationItem({super.key});
+  const TransationItem({super.key, required this.transationHistoryModel});
+  final TransationHistoryModel transationHistoryModel;
 
   @override
   Widget build(BuildContext context) {
-    return 
+    return Card(
+      color: const Color(0xffFAFAFA),
+      child: ListTile(
+        title: Text(
+          transationHistoryModel.title,
+          style: AppStyle.styleSemibold16,
+        ),
+        subtitle: Text(
+          transationHistoryModel.subTitle,
+          style: AppStyle.styleRegular16,
+        ),
+        trailing: Text(
+          transationHistoryModel.traling,
+          style: AppStyle.styleSemibold20,
+        ),
+      ),
+    );
   }
 }
