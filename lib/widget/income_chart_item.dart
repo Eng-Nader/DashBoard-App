1eb759fd5147f7bf17e18aad0da07d47fc1f1332
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-14 10:51:10 +0300

    installing a fl chart peckage

---
+++ lib/widget/income_chart_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IncomeChartItem extends StatelessWidget {
+  const IncomeChartItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  
+  }
+}
