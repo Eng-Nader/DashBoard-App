78bb782b6074caf030ff46c0a7a18d93058881be
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-14 09:58:57 +0300

    create A IncomeChart

--- lib/widget/desktop_layout.dart
+++ lib/widget/desktop_layout.dart
@@ -1,5 +1,7 @@
 import 'package:dashboarda_app/widget/admin_payment.dart';
+import 'package:dashboarda_app/widget/custom_back_ground_container.dart';
 import 'package:dashboarda_app/widget/custom_drawar.dart';
+import 'package:dashboarda_app/widget/income_char.dart';
 import 'package:dashboarda_app/widget/quick_invoice_and_expenses.dart';
 import 'package:flutter/material.dart';
 
@@ -24,9 +26,17 @@ class DesptopLayout extends StatelessWidget {
           width: 24,
         ),
         Expanded(
-          child: AdminPaymnet(),
-        ),
+            child: Column(
+          children: [
+            AdminPaymnet(),
+            SizedBox(
+              height: 20,
+            ),
+            IncomeChart(),
+          ],
+        )),
       ],
     );
   }
 }
+
---
+++ lib/widget/income_char.dart
@@ -0,0 +1,12 @@
+import 'package:dashboarda_app/widget/custom_back_ground_container.dart';
+import 'package:flutter/material.dart';
+
+class IncomeChart extends StatelessWidget {
+  const IncomeChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomBackGroundContainer(
+        paddingValue: 20, child: Text('flutter demo'));
+  }
+}
