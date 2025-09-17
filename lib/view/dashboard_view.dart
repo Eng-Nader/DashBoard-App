1e643c2a8518e5969499f515b2fca768ba73f29b
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-17 14:11:16 +0300

    create a custom darwer in mobile layout and open it

--- lib/view/dashboard_view.dart
+++ lib/view/dashboard_view.dart
@@ -1,15 +1,37 @@
+import 'package:dashboarda_app/widget/custom_drawar.dart';
 import 'package:dashboarda_app/widget/desktop_layout.dart';
 import 'package:dashboarda_app/widget/adaptive_layout.dart';
 import 'package:dashboarda_app/widget/mobile_layout.dart';
 import 'package:dashboarda_app/widget/tablet_layout.dart';
 import 'package:flutter/material.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
 
   @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  final GlobalKey<ScaffoldState> key = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: key,
+      drawer:
+          MediaQuery.of(context).size.width < 800 ? const CustomDrawar() : null,
+      appBar: MediaQuery.of(context).size.width < 800
+          ? AppBar(
+              leading: IconButton(
+                onPressed: () {
+                  key.currentState!.openDrawer();
+                },
+                icon: const Icon(Icons.menu),
+              ),
+              elevation: 0,
+              backgroundColor: Colors.white,
+            )
+          : null,
       backgroundColor: const Color(0xffF7F9FA),
       body: AdaptiveLayout(
         moibleLayout: (context) => const MobileLayout(),
