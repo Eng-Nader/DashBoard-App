c803da68955c9b3a866ca664d3df31d878037af0
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-17 14:14:14 +0300

    Refactor in Custom drawer

--- lib/widget/custom_drawar.dart
+++ lib/widget/custom_drawar.dart
@@ -12,6 +12,7 @@ class CustomDrawar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.of(context).size.width * .7,
       decoration: const BoxDecoration(
         color: Colors.white,
       ),
