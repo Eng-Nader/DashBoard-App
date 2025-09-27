6ed9faddad0f552c30b79ff386b9defd8bc2d872
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-28 00:21:21 +0300

    create a Resoponseve font Size File

--- lib/core/utils/app_style.dart
+++ lib/core/utils/app_style.dart
@@ -67,25 +67,3 @@ abstract class AppStyle {
   );
 }
 
-double getResponseveText(double fontSize) {
-  double scaleFactor = getScaleFactor();
-
-  double responseveText = fontSize * scaleFactor;
-  double lowerLimit = fontSize * .8;
-  double upperLimit = fontSize * 1.2;
-  return responseveText.clamp(lowerLimit, upperLimit);
-}
-
-double getScaleFactor() {
-  var dispacther = PlatformDispatcher.instance;
-  var pyscialWidth = dispacther.views.first.physicalSize.width;
-  var devicePixelRatio = dispacther.views.first.devicePixelRatio;
-  var width = pyscialWidth / devicePixelRatio;
-  if (width < SizeConfig.tablet) {
-    return width / 500;
-  } else if (width < SizeConfig.desktop) {
-    return width / 1000;
-  } else {
-    return width / 1920;
-  }
-}
